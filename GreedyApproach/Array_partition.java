e97356bd14f86e01045cfb5f0c8ab7df4a810e10
Shubham <shubhammishra780808@gmail.com>
2025-07-03 10:29:19 +0530

    now i'm also add Array Partition problem

--- GreedyApproach/Array_partition.java
+++ GreedyApproach/Array_partition.java
@@ -1,5 +1,16 @@
+import java.util.Arrays;
+
 public class Array_partition {
+  public static int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int max_sum =0;
+        for(int i=0;i<nums.length-1;i+=2){
+            max_sum = max_sum + Math.min(nums[i],nums[i+1]);
+        }
+        return max_sum;
+    }
   public static void main(String[] args) {
-    
+    int nums[] = {1,4,3,2};
+    arrayPairSum(nums);
   }
 }
